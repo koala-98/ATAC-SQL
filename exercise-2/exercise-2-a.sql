843fad47dd0e509018521f6a0a04deb2ef5e86f1
koala-98 <dangkhoa270998@gmail.com>
2025-03-22 15:39:46 +0700

    Fix exercise 2-a

--- exercise-2/exercise-2-a.sql
+++ exercise-2/exercise-2-a.sql
@@ -1,10 +1,10 @@
 -- exercise 2-a: Tinh tong doanh thu(UnitPrice*Quantity) cua tung khach hang dua tren bang Orders va Order Details
 /*EXPLAIN: 
 - SUM(od.UnitPrice * od.Quantity) Tinh tong doanh thu cua khach hang, trong bang Order Details, OrderID xuat hien nhieu lan, vay nen dung
-SUM de cong don cho moi khach han
-- LEFT JOIN do ben bang Order Details nhieu OrderID co gia tri NULL*/
+SUM de cong don cho moi khach han*/
 
-SELECT o.OrderID, o.CustomerID, SUM(od.UnitPrice * od.Quantity) AS TotalRevenue
+SELECT o.CustomerID, SUM(od.UnitPrice * od.Quantity) AS TotalRevenue
 FROM Orders o
-LEFT JOIN "Order Details" od ON o.OrderID = od.OrderID
-GROUP BY o.OrderID;
+JOIN "Order Details" od ON o.OrderID = od.OrderID
+JOIN Customers c ON o.CustomerID = c.CustomerID
+GROUP BY c.CustomerID;
