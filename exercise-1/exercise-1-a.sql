37d9b0962f3d1c6fa1ac3ee75907e68e119a55e6
koala-98 <dangkhoa270998@gmail.com>
2025-03-27 20:37:13 +0700

    Fix exercise 1-a

--- exercise-1/exercise-1-a.sql
+++ exercise-1/exercise-1-a.sql
@@ -1,7 +1,5 @@
 -- exercise 1-a: Liet ke tat ca Products cung voi UnitPrice va UnitsInStock
 /*EXPLAIN: Self-join được sử dụng để so sánh các dòng dữ liệu trong cùng một bảng, sử dụng <> để đảm bảo rằng ProductID là khác nhau*/ 
  
-SELECT p1.ProductID, p1.ProductName, p1.UnitPrice, p1.UnitsInStock
-FROM Products p1
-JOIN Products p2 ON p1.UnitPrice = p2.UnitPrice AND p1.UnitsInStock = p2.UnitsInStock
-WHERE p1.ProductID <> p2.ProductID;
+SELECT ProductID, ProductName, UnitPrice, UnitsInStock
+FROM Products;
